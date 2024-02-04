ffad5686e5fb9c92546215924ca4342d00464037
Alicia <aliciaboonepro@gmail.com>
2024-02-04 17:53:45 -0500

    Program structure commit

---
+++ frontend/src/assets/data/faq.js
@@ -0,0 +1,8 @@
+export const faqs = [
+  {
+    question: "Is there a deposit fee?",
+    content: "Yes, there is a deposit fee to book an appointment. The deposit fee is $25.",
+  }
+
+  //add more later
+]
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#1BB28C",
+        yellowColor: "#FED45B",
+        grayWhiteColor: "#EFEEEA",
+        salmonColor: "E86A58",
+        headingColor: "#23362B",
+        textColor: "#9BC7C5",
+      },
+
+      boxShadow: {
+        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",
+      },
+    },
   },
   plugins: [],
 }
